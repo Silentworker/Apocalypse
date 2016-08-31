3cc6dc2517b6cd3db7424360efb45ac324ac24fd
silentworker <silentworker@yandex.ua>
2016-08-31 23:15:09 +0300

    Commands start.

---
+++ Assets/Scripts/core/command/SequenceMacro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Object = UnityEngine.Object;
+
+namespace Assets.Scripts.core.command
+{
+    public class SequenceMacro : AsyncCommand, ISequenceMacro
+    {
+        private List<ICommand> _commands;
+
+        public override void Execute(Object data = null)
+        {
+            base.Execute();
+        }
+
+        public void Add(ICommand comand)
+        {
+        }
+
+        public void Remove(ICommand comand)
+        {
+        }
+
+        public void Cancel()
+        {
+        }
+    }
+}
